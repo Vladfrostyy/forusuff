0ece9c43e16bea74784b25f9482896add93891eb
Vladfrostyy <92669220+Vladfrostyy@users.noreply.github.com>
2022-07-22 12:05:51 +0300

    f
    fs

---
+++ js/script.js
@@ -0,0 +1,7 @@
+alert('Hello usuf');
+confirm('If your name Usuf --- touch{yes}');
+
+alert('Please, click in "Hello usuf"');
+ function myFunction(){
+	alert("Thanks for click, now your PC has a virus!")
+}
